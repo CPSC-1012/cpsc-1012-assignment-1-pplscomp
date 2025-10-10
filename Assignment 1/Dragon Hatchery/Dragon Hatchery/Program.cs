9a55f688c1fd610627e68107ba63bf75434c79d7
pplscomp <cmalone3@nait.ca>
2025-10-10 17:16:08 -0600

    First Committ, entered in Project Layout

---
+++ Assignment 1/Dragon Hatchery/Dragon Hatchery/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Declare Variables
+using System.Text;
+
+///<summary>
+/// Dragon Hatchery Simulator
+/// This is meant to answer how many dragons will be in the Hatchery after a certian time period is provided, as well as a rate that the dragons produce.
+/// Created By: Chris Malone
+/// Date: Sept 10, 2025
+/// </summary>
+
+//Declare Variables
+
+
+//Display Title
+
+
+//Prompt for inputs
+//Prompt for Initial Amount of Dragons (intialDragons)
+
+
+//Prompt for Rate of Dragon's increase Rate. totalRate = (100 + Rate)/100
+
+//Prompt for Time of production (in weeks). totalTime = 7*weeks
+
+//Calculate how many dragons at end of provided time. totalDragons = initialDragons + (totalRate)^totalTime
+//Calculate dragonRateTime = (totalRate)^totalTime
+
+//Calculate totalDragons = initialDragon+dragonRateTime
+
+//Get totalDragons rounded.
+
+//Diplay amount of Dragons rounded to whole number. 
+
+//End
+
