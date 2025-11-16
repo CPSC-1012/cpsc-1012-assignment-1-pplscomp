0752f0e340e400a5fca9e08908904a5dac62e8f2
pplscomp <cmalone3@nait.ca>
2025-11-15 21:59:28 -0700

    Seeing if program works

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,419 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        ///<summary>
+        ///Program to run as a monthly time tracker, to be able to load, save, and display data entered.
+        ///Created By: Chris Malone
+        ///Created For: CPSC1012 - Abhilash Hareendranathan
+        ///Date: November 15, 2025
+        ///</summary>
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+
+            // TODO: 
+            const int DayMaxLength = 31; //maximum days to a month
+            const int MonthMaxLength = 12; //maximum months to a year
+            const int MinuteMaxLength = 1440; //maximum minutes to a day
+            const int YearMaxLength = 1; //only for 1 year
+
+            // declare a constant to represent the maximum size of the arrays
+            // arrays must be large enough to store data for an entire month 
+
+            // TODO:
+            // create a string array named dates, using the max size constant you created above to specify the physical size of the array
+            string[] month = new string[MonthMaxLength] { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+            string[] day = new string[DayMaxLength] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
+            String[] year = new string[YearMaxLength] { "2025" };
+            string[,,] dates = new string[YearMaxLength, MonthMaxLength, DayMaxLength];
+
+            // TODO:
+
+            // create a double array named minutes, using the max size constant you created above to specify the physical size of the array
+            double[] minutes = new double[MinuteMaxLength];
+            // TODO:
+            // create a variable to represent the logical size of the array
+            int minutesLogicalSize = minutes.Length - 1;
+
+
+
+            DisplayProgramIntro();
+
+            DisplayMainMenu();
+
+            while (continueProgram)
+            {
+                string mainMenuChoice = Prompt("Enter MAIN MENU option ('D' to display menu): ").ToUpper();
+                Console.WriteLine();
+
+                //MAIN MENU Switch statement
+                switch (mainMenuChoice)
+                {
+                    case "N": //[N]ew Daily Entries
+
+                        if (AcceptNewEntryDisclaimer())
+                        {
+                            // TODO: call EnterDailyValues & assign its return value
+                            Console.WriteLine($"\nEntries completed. {count} records in temporary memory.\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cancelling new data entry. Returning to MAIN MENU.");
+                        }
+                        break;
+                    case "S": //[S]ave Entries to File
+                        if (count == 0)
+                        {
+                            Console.WriteLine("Sorry, LOAD data or enter NEW data before SAVING.");
+                        }
+                        else if (AcceptSaveEntryDisclaimer())
+                        {
+                            string filename = PromptForFilename();
+                            // TODO: call SaveToFile()
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cancelling save operation. Returning to MAIN MENU.");
+                        }
+
+                        break;
+                    case "E": //[E]dit Entries
+                        if (count == 0)
+                        {
+                            Console.WriteLine("Sorry, LOAD data or enter NEW data before EDITING.");
+                        }
+                        else if (AcceptEditEntryDisclaimer())
+                        {
+                            //TODO: call EditEntries()
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cancelling EDIT operation. Returning to MAIN MENU.");
+                        }
+                        break;
+                    case "L": //[L]oad  File
+                        if (AcceptLoadEntryDisclaimer())
+                        {
+                            string filename = Prompt("Enter name of file to load: ");
+                            // TODO: call LoadFromFile() and assign its return value
+                            Console.WriteLine($"{count} records were loaded.\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cancelling LOAD operation. Returning to MAIN MENU.");
+                        }
+                        break;
+                    case "V":
+                        if (count == 0)
+                        {
+                            Console.WriteLine("Sorry, LOAD data or enter NEW data before VIEWING.");
+                        }
+                        else
+                        {
+                            // TODO: call DisplayEntries()
+                        }
+                        break;
+                    case "M": //[M]onthly Statistics
+                        if (count == 0)
+                        {
+                            Console.WriteLine("Sorry, LOAD data or enter NEW data before ANALYSIS.");
+                        }
+                        else
+                        {
+                            RunAnalysisMenu(dates, minutes, count);
+                        }
+                        break;
+                    case "D": //[D]isplay Main Menu
+                        //TODO: call DisplayMainMenu()
+                        break;
+                    case "Q": //[Q]uit Program
+                        bool quit = Prompt("Are you sure you want to quit (y/n)? ").ToLower().Equals("y");
+                        Console.WriteLine();
+                        if (quit)
+                        {
+                            continueProgram = false;
+                        }
+                        break;
+                    default: //invalid entry. Reprompt.
+                        Console.WriteLine("Invalid reponse. Enter one of the letters to choose a menu option.");
+                        break;
+                }
+            }
+
+            DisplayProgramOutro();
+        }
+
+        /// <summary>
+        /// Runs the analysis sub-menu to display summary metrics.
+        /// </summary>
+        /// <param name="dates">an array containing dates in YYYY-MM-DD format</param>
+        /// <param name="numbers">an array containing numeric values</param>
+        /// <param name="count">logical count of elements</param>
+        static void RunAnalysisMenu(string[] dates, double[] numbers, int count)
+        {
+            bool runAnalysis = true;
+            string year = dates[0].Substring(0, 4),
+                month = dates[0].Substring(5, 3);
+
+            while (runAnalysis)
+            {
+                string analysisMenuChoice;
+
+                DisplayAnalysisMenu();
+
+                analysisMenuChoice = Prompt("Enter ANALYSIS sub-menu option: ").ToUpper();
+                Console.WriteLine();
+
+                switch (analysisMenuChoice)
+                {
+                    case "A": //[A]verage 
+                        // TODO: uncomment the next 2 lines & call CalculateMean();
+                        //double mean = ;
+                        //Console.WriteLine($"The mean value for {month} {year} is: {mean:N2}.\n");
+                        break;
+                    case "H": //[H]ighest 
+                        // TODO: uncomment the next 2 lines & call CalculateLargest();
+                        //double largest = ;
+                        //Console.WriteLine($"The largest value for {month} {year} is: {largest:N2}.\n");
+                        break;
+                    case "L": //[L]owest 
+                        //TODO: uncomment the next 2 lines & call CalculateSmallest();
+                        //double smallest = ;
+                        //Console.WriteLine($"The smallest value for {month} {year} is: {smallest:N2}.\n");
+                        break;
+                    case "G": //[G]raph 
+                        //TODO: call DisplayChart()
+                        Prompt("Press <enter> to continue...");
+                        break;
+                    case "R": //[R]eturn to MAIN MENU
+                        runAnalysis = false;
+                        break;
+                    default: //invalid entry. Reprompt.
+                        Console.WriteLine("Invalid reponse. Enter one of the letters to choose a submenu option.");
+                        break;
+                }
+            }
+        }
+
+        // ================================================================================================ //
+        //                                                                                                  //
+        //                                              METHODS                                             //
+        //                                                                                                  //
+        // ================================================================================================ //
+
+        // ++++++++++++++++++++++++++++++++++++ Difficulty 1 ++++++++++++++++++++++++++++++++++++
+
+        // TODO: create the DisplayMainMenu() method
+        /// <summary>
+        /// Displays the main menu for the program.
+        /// </summary>
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("[N]ew Daily Entries");
+            Console.WriteLine{
+                "[S]ave Entries to File");
+                Console.WriteLine("[E]dit Entries");
+                Console.WriteLine("[L]oad File");
+                Console.WriteLine("[V]iew Entered/Loaded Data");
+                Console.WriteLine("[M]onthly Statistics");
+                Console.WriteLine("[D]isplay Main Menu");
+                Console.WriteLine("[Q]uit Program");
+            }
+
+
+            // TODO: create the DisplayAnalysisMenu() method
+            /// <summary>
+            /// Displays the submenu for data analysis for the program.
+            /// </summary>
+            static void DisplayAnalysisMenu()
+            {
+                Console.WriteLine("[A]verage");
+                Console.WriteLine("[H]ighest");
+                Console.WriteLine("[L]owest");
+                Console.WriteLine("[G]raph");
+                Console.WriteLine("[R]eturn to MAIN MENU");
+            }
+
+            // TODO: create the Prompt method
+            /// <summary>
+            /// Allows to Prompt the user with the customized strings.
+            /// </summary>
+            /// <param name = "text"> text to be prompted to the user</param>
+
+
+            static void Prompt(string text)
+            {
+                string text;
+                Console.WriteLine("${text}");
+            }
+
+            // TODO: create the PromptDouble() method
+
+            /// <summary>
+            /// Allows to Prompt the user for a value classified as a double.
+            /// </summary>
+            /// <param name = "value"> value to be interpreted as a double</param>
+            static void PromptDouble(double value)
+            {
+                double value;
+                Console.Write("Enter a Value");
+                double.Parse(Console.ReadLine());
+            }
+
+            // optional TODO: create the PromptInt() method
+
+            /// <summary>
+            /// Allows to Prompt the user for a value classified as a integer.
+            /// </summary>
+            /// <param name = "value"> value to be interpreted as an integer</param>
+            static void PromptInt(int value)
+            {
+                int value;
+                Console.Write("Enter a Value");
+                int.Parse(Console.ReadLine());
+
+                // TODO: create the CalculateLargest() method
+
+                // TODO: create the CalculateSmallest() method
+
+                // TODO: create the CalculateMean() method
+
+                // ++++++++++++++++++++++++++++++++++++ Difficulty 2 ++++++++++++++++++++++++++++++++++++
+
+                // TODO: create the EnterDailyValues method
+
+                // TODO: create the LoadFromFile method
+
+                // TODO: create the SaveToFile method
+
+                // TODO: create the DisplayEntries method
+
+                // ++++++++++++++++++++++++++++++++++++ Difficulty 3 ++++++++++++++++++++++++++++++++++++
+
+                // TODO: create the EditEntries method
+
+                // ++++++++++++++++++++++++++++++++++++ Difficulty 4 ++++++++++++++++++++++++++++++++++++
+
+                // TODO: create the DisplayChart method
+
+                // ********************************* Helper methods *********************************
+
+                /// <summary>
+                /// Displays the Program intro.
+                /// </summary>
+                static void DisplayProgramIntro()
+        {
+            Console.WriteLine("****************************************\n" +
+                "*                                      *\n" +
+                "*          Monthly  Game Time          *\n" +
+                "*                                      *\n" +
+                "****************************************\n");
+        }
+
+        /// <summary>
+        /// Displays the Program outro.
+        /// </summary>
+        static void DisplayProgramOutro()
+        {
+            Console.Write("Program terminated. Press ENTER to exit program...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Displays a disclaimer for NEW entry option.
+        /// </summary>
+        /// <returns>Boolean, if user wishes to proceed (true) or not (false).</returns>
+        static bool AcceptNewEntryDisclaimer()
+        {
+            bool response;
+            Console.WriteLine("Disclaimer: proceeding will overwrite all unsaved data.\n" +
+                "Hint: Select EDIT from the main menu instead, to change individual days.\n");
+            response = Prompt("Do you wish to proceed anyway? (y/n) ").ToLower().Equals("y");
+            Console.WriteLine();
+            return response;
+        }
+
+        /// <summary>
+        /// Displays a disclaimer for SAVE entry option.
+        /// </summary>
+        /// <returns>Boolean, if user wishes to proceed (true) or not (false).</returns>
+        static bool AcceptSaveEntryDisclaimer()
+        {
+            bool response;
+            Console.WriteLine("Disclaimer: saving to an EXISTING file will overwrite data currently on that file.\n" +
+                "Hint: Files will be saved to this program's directory by default.\n" +
+                "Hint: If the file does not yet exist, it will be created.\n");
+            response = Prompt("Do you wish to proceed anyway? (y/n) ").ToLower().Equals("y");
+            Console.WriteLine();
+            return response;
+        }
+
+        /// <summary>
+        /// Displays a disclaimer for EDIT entry option.
+        /// </summary>
+        /// <returns>Boolean, if user wishes to proceed (true) or not (false).</returns>
+        static bool AcceptEditEntryDisclaimer()
+        {
+            bool response;
+            Console.WriteLine("Disclaimer: editing will overwrite unsaved values.\n" +
+                "Hint: Save to a file before editing.\n");
+            response = Prompt("Do you wish to proceed anyway? (y/n ").ToLower().Equals("y");
+            Console.WriteLine();
+            return response;
+        }
+
+        /// <summary>
+        /// Displays a disclaimer for LOAD entry option.
+        /// </summary>
+        /// <returns>Boolean, if user wishes to proceed (true) or not (false).</returns>
+        static bool AcceptLoadEntryDisclaimer()
+        {
+            bool response;
+            Console.WriteLine("Disclaimer: proceeding will overwrite all unsaved data.\n" +
+                "Hint: If you entered New Daily entries, save them first!\n");
+            response = Prompt("Do you wish to proceed anyway? (y/n) ").ToLower().Equals("y");
+            Console.WriteLine();
+            return response;
+        }
+
+        /// <summary>
+        /// Displays prompt for a filename, and returns a valid filename. 
+        /// Includes exception handling.
+        /// </summary>
+        /// <returns>User-entered string, representing valid filename (.txt or .csv)</returns>
+        static string PromptForFilename()
+        {
+            string filename = "";
+            bool isValidFilename = true;
+            const string CSV_FILE_EXTENSION = ".csv";
+            const string TXT_FILE_EXTENSION = ".txt";
+
+            do
+            {
+                filename = Prompt("Enter name of .csv or .txt file to save to (e.g. JAN-2025-data.csv): ");
+                if (filename == "")
+                {
+                    isValidFilename = false;
+                    Console.WriteLine("Please try again. The filename cannot be blank or just spaces.");
+                }
+                else
+                {
+                    if (!filename.EndsWith(CSV_FILE_EXTENSION) && !filename.EndsWith(TXT_FILE_EXTENSION)) //if filename does not end with .txt or .csv.
+                    {
+                        filename = filename + CSV_FILE_EXTENSION; //append .csv to filename
+                        Console.WriteLine("It looks like your filename does not end in .csv or .txt, so it will be treated as a .csv file.");
+                        isValidFilename = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("It looks like your filename ends in .csv or .txt, which is good!");
+                        isValidFilename = true;
+                    }
+                }
+            } while (!isValidFilename);
+            return filename;
+        }
+
+    }
+}
