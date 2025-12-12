b031c1c3106ad3291e73117ba1fbf174d181ee36
pplscomp <cmalone3@nait.ca>
2025-12-12 15:23:49 -0700

    Initial Push Project Start summary notes inserted.

---
+++ Assignment4/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,8 @@
+﻿///<summary>
+/// Program created to allow staff at "Thrilladelphia" to store and manage details about each ride.
+/// Storing details such as: How thrilling a ride is, how many visitors it had, how profitable it is, and/or how terrifying it is.
+/// Created By: Chris Malone
+/// Created for: CPSC1012 - Abhilash Hareendranathan
+/// Created: Dec 12th, 2025
+/// Last updated: Dec 12, 2025 @ 1523 MST
+/// </summary>
